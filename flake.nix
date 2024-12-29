df80b70f9706df14d344e4dd7417cef8fac5c7a1
htn332805 <96609281+htn332805@users.noreply.github.com>
2024-12-29 00:06:45 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "NixOS configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-24.11";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-24.05";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.nur.url = "github:nix-community/NUR";
@@ -82,12 +82,22 @@
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
 
       nixosConfigurations.nixos = mkNixosConfiguration {
-        hostname = "nixos";
+        hostname = "wsl";
         username = "nixos"; # FIXME: replace with your own username!
         modules = [
           nixos-wsl.nixosModules.wsl
           ./wsl.nix
         ];
+      }; #end of wsl configuration
+
+      nixosConfigurations.nixos = mkNixosConfiguration {
+        hostname = "PI4s";
+        username = "nixos"; # FIXME: replace with your own username!
+        modules = [
+          nixpkgs.nixosModules.pi4
+          ./pi4.nix
+        ];
       };
+
     };
 }
