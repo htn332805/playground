7838692e1bc67d7abd870d7ee14f2a89b6faf956
htn332805 <96609281+htn332805@users.noreply.github.com>
2024-12-29 00:25:10 -0800

    Update pi4.nix

--- pi4.nix
+++ pi4.nix
@@ -22,14 +22,15 @@
   security.sudo.wheelNeedsPassword = false;
 
   # FIXME: uncomment the next line to enable SSH
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   users.users.${username} = {
     isNormalUser = true;
+    initialPassword = "hai";
     # FIXME: change your shell here if you don't want fish
     shell = pkgs.fish;
     extraGroups = [
-      "wheel"
+      "wheel" "networkmanger"
       # FIXME: uncomment the next line if you want to run docker without sudo
       # "docker"
     ];
@@ -47,19 +48,7 @@
     ];
   };
 
-  system.stateVersion = "22.05";
-
-  wsl = {
-    enable = true;
-    wslConf.automount.root = "/mnt";
-    wslConf.interop.appendWindowsPath = false;
-    wslConf.network.generateHosts = false;
-    defaultUser = username;
-    startMenuLaunchers = true;
-
-    # Enable integration with Docker Desktop (needs to be installed)
-    docker-desktop.enable = false;
-  };
+  system.stateVersion = "24.05";
 
   virtualisation.docker = {
     enable = true;
@@ -67,24 +56,7 @@
     autoPrune.enable = true;
   };
 
-  # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
-  # solution adapted from: https://github.com/K900/vscode-remote-workaround
-  # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
-  # systemd.user = {
-  #   paths.vscode-remote-workaround = {
-  #     wantedBy = ["default.target"];
-  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
-  #   };
-  #   services.vscode-remote-workaround.script = ''
-  #     for i in ~/.vscode-server/bin/*; do
-  #       if [ -e $i/node ]; then
-  #         echo "Fixing vscode-server in $i..."
-  #         ln -sf ${pkgs.nodejs_18}/bin/node $i/node
-  #       fi
-  #     done
-  #   '';
-  # };
-
+  
   nix = {
     settings = {
       trusted-users = [username];
@@ -115,7 +87,7 @@
 
     gc = {
       automatic = true;
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 30d";
     };
   };
 }
