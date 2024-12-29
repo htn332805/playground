58cc03ec8552bcd20f113f15a27323496ed0e33d
htn332805 <96609281+htn332805@users.noreply.github.com>
2024-12-29 02:03:49 -0800

    Update disk-config.nix

--- disk-config.nix
+++ disk-config.nix
@@ -2,7 +2,7 @@
 {lib, ...}: {
   disko.devices = {
     disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
+      device = lib.mkDefault "/dev/disk/by-id/wwn-0x500a07512e906819";
       type = "disk";
       content = {
         type = "gpt";
@@ -11,8 +11,8 @@
             name = "boot";
             size = "1M";
             type = "EF02";
-          };
-          esp = {
+          }; # end of boot part
+          ESP = {
             name = "ESP";
             size = "500M";
             type = "EF00";
@@ -20,36 +20,44 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-            };
-          };
-          root = {
+              mountOptions = [ "umask=0077" ];
+            }; #end of ESP Content
+          }; #end of ESP part
+          zfs = {
             name = "root";
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
+              type = "zfs";
+              pool = "rpool";
+            }; #end of zfs content
+          }; #end of zfs part
+        }; #end of all the partitions
+      }; #end of the disk content
+    }; #end of disk config layout
+    zpool = {
+      rpool = {
+        type = "zpool";
+        # Workaround: cannot import 'zroot': I/O error in disko tests
+        options.cachefile = "none";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+          atime = "off";
+          recordsize = "1M";
+          xattr = "sa";
+          acltype = "posixacl";
+          ashift = "12";
+        };#end of rootFsOptions
+        mountpoint = "/";
+        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+        datasets = {
+          zfs_fs = {
+            type = "zfs_fs";
+            mountpoint = "/zfs_fs";
+            options."com.sun:auto-snapshot" = "true";
           };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
-            };
-          };
-        };
-      };
-    };
-  };
-}
+        
+      }; #end of datasets
+    }; #end of rpool
+  }; #end of zpool
+} #end of disko nix
