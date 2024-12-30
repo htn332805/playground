0f4f9f3a4a4afbd4bd70c35a2e3a2847fcdcd9bd
htn332805 <96609281+htn332805@users.noreply.github.com>
2024-12-30 12:30:30 -0800

    Update home.nix

--- home.nix
+++ home.nix
@@ -141,18 +141,18 @@ in {
         side-by-side = true;
         navigate = true;
       };
-      userEmail = ""; # FIXME: set your git email
-      userName = ""; #FIXME: set your git username
+      userEmail = "htn332805@hotmail.com"; # FIXME: set your git email
+      userName = "htn332805"; #FIXME: set your git username
       extraConfig = {
         # FIXME: uncomment the next lines if you want to be able to clone private https repos
-        # url = {
-        #   "https://oauth2:${secrets.github_token}@github.com" = {
-        #     insteadOf = "https://github.com";
-        #   };
+         url = {
+           "https://oauth2:${secrets.github_token}@github.com" = {
+             insteadOf = "https://github.com";
+           };
         #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
         #     insteadOf = "https://gitlab.com";
         #   };
-        # };
+         }; #end of extraConfig.url
         push = {
           default = "current";
           autoSetupRemote = true;
